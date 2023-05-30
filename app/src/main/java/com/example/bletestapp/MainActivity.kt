332e22e207e530b0dcaa856b5c2d0326cffc9a3a
Raphael Kathambana <maya12raph@gmail.com>
2023-05-30 20:27:54 +0300

    BlueTooth Updated Connection

--- app/src/main/java/com/example/bletestapp/MainActivity.kt
+++ app/src/main/java/com/example/bletestapp/MainActivity.kt
@@ -1,48 +1,79 @@
 package com.example.bletestapp
 
-import android.Manifest
+import android.Manifest.permission.BLUETOOTH_ADMIN
+import android.Manifest.permission.BLUETOOTH_CONNECT
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.RequestPermission
 import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
+    private var btPermission = false
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnScanBt = findViewById<Button>(R.id.connectBtn)
+        btnScanBt.setOnClickListener {
+            Log.d("BT BUTTON CLICKED", "Scan Button Clicked")
+            scanBluetooth(btnScanBt)
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun scanBluetooth(view: View) {
         val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
         val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
         if (bluetoothAdapter == null) {
-            Log.d("NO BT SUPPORT", "Does not Support Bluetooth")
-        }
-        if (bluetoothAdapter?.isEnabled == false) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            Log.d("BT Support", "Noice")
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.BLUETOOTH_CONNECT
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return
+            Toast.makeText(this, "Device Doesn't Support Bluetooth", Toast.LENGTH_LONG).show()
+        } else {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                bluetoothPermissionLauncher.launch(BLUETOOTH_CONNECT)
+            } else {
+                bluetoothPermissionLauncher.launch(BLUETOOTH_ADMIN)
             }
-            val REQUEST_ENABLE_BT = -1
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
         }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private  val bluetoothPermissionLauncher = registerForActivityResult(
+        RequestPermission()
+    ){isGranted: Boolean ->
+        if (isGranted) {
+            val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+            val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.adapter
+            btPermission = true
+            if (bluetoothAdapter?.isEnabled == false) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                btActivityResultLauncher.launch(enableBtIntent)
+            } else {
+                btScan()
+            }
+        } else {
+            btPermission = false
+        }
+    }
+    @RequiresApi(Build.VERSION_CODES.M)
+    private  val btActivityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){result: ActivityResult ->
+        if (result.resultCode == RESULT_OK) {
+        btScan()
+        }
+    }
+
+    private fun btScan() {
+        Toast.makeText(this, "BlueTooth Connected Successfully", Toast.LENGTH_LONG).show()
+        Log.d("BT CONNECTED", "Bluetooth Connected")
     }
 //    private val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
 //    private var scanning = false
